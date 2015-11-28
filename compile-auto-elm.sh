fee558eb1d42b18d9401e314517da15a9250d617
Virgilio T. Dato, Jr <virgilio_dato@yahoo.com>
2015-11-28 20:50:09 +0800

    making modularized app failed

--- compile-auto-elm.sh
+++ compile-auto-elm.sh
@@ -1,2 +1,3 @@
 echo 'watching elm file'
-filewatcher elm/main.elm 'elm-make elm/main.elm --output public/js/main.js'
+cd src && filewatcher '**/*.elm' 'elm-make main.elm --output ../public/js/main.js'
+cd ../..
--- compile-auto-less.sh
+++ compile-auto-less.sh
@@ -1,5 +1,5 @@
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 echo 'watching less file'
-filewatcher less/main.less 'lessc less/main.less > public/css/main.css'
+filewatcher less/main.less 'lessc less/main.less > public/css/main.css && echo "compiled css"'
 
 
