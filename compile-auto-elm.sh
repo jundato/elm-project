16b7560572c17bc341ee39c15c0241babbb5f225
Virgilio T. Dato, Jr <virgilio_dato@yahoo.com>
2015-11-28 21:35:34 +0800

    upgraded to 0.16

--- compile-auto-elm.sh
+++ compile-auto-elm.sh
@@ -1,2 +1,2 @@
 echo 'watching elm file'
-filewatcher elm/main.elm 'elm-make elm/main.elm --output public/js/main.js'
+cd src && filewatcher main.elm 'elm-make main.elm --output ../public/js/main.js'
